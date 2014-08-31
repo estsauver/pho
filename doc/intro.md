b11d94ba4ecc739bacc9a0a568e82771d846e405
Earl St Sauver <estsauver@gmail.com>
2014-08-31 16:50:32 -0700

    Initial Commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to pho
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/pho/core_test.clj
@@ -0,0 +1,7 @@
+(ns pho.core-test
+  (:require [clojure.test :refer :all]
+            [pho.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
